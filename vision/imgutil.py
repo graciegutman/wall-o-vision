d0b3825a0e15267088f5f5ceca22a119e62abefc
Gracie Gutman <gracie@runscope.com>
2016-04-30 22:35:35 -0700

    A couple more util functions. Resizing and opening a list of images with arbitrary extension

--- vision/imgutil.py
+++ vision/imgutil.py
@@ -1,4 +1,19 @@
 from PIL import Image
+from pylab import array, uint8
+import os
 
 def to_grayscale(filepath):
     return Image.open(filepath).convert('L')
+
+
+def get_img_list(dirpath, extension):
+    img_list = []
+    for f in os.listdir(dirpath):
+        if f.endswith(extension):
+            img_list.append(os.path.join(dirpath, f))
+    return img_list
+
+
+def resize(img_array, width, height):
+    img = Image.fromarray(uint8(img_array))
+    return array(img.resize((width, height)))
